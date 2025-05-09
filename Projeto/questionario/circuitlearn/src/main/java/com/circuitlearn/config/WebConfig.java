d0726c61efb903ce1326a9a893fabc6289a08723
Mateus Bernardi Alves <mateusbnalves@gmail.com>
2025-05-09 09:01:22 -0300

    Questionario Config
    Classe config para configurar cors

---
+++ Projeto/questionario/circuitlearn/src/main/java/com/circuitlearn/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.circuitlearn.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Apply to all /api endpoints
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500") // Allow your frontend's origin (Live Server default for VS Code)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
