e4251dcc4ffa33b8325c0058d1767f4e0369d162
Mateus Bernardi Alves <mateusbnalves@gmail.com>
2025-05-09 08:37:15 -0300

    Pagina Questionario
    Arquivos iniciais do springboot

---
+++ Projeto/questionario/circuitlearn/src/main/java/com/circuitlearn/CircuitlearnApplication.java
@@ -0,0 +1,13 @@
+package com.circuitlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitlearnApplication.class, args);
+	}
+
+}
---
+++ Projeto/questionario/circuitlearn/src/test/java/com/circuitlearn/CircuitlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circuitlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
